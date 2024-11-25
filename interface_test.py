2d98e17ccaf5d5094a33d0023905d6319de81f53
kenzokrizz <kenechukwu029@revature.net>
2024-11-24 21:13:47 -0500

    new stuff added

--- interface_test.py
+++ interface_test.py
@@ -2,52 +2,86 @@ import sys # Importing the sys module to use sys.exit()
 import time
 import csv
 import json
-user_path = input("enter path to data file: ")
 
-def read_file():
+
+def read_file(user_path):
     #code to read file 
-    while True:
-        user_path = input("enter path to file (csv/json): ").strip()
-
+    while True:        
+        data = [] 
         try:
             if user_path.endswith('.csv'):
-                with open(user_path, 'r') as file:
-                    reader = csv.DictReader(file)
-                    data = [row for row in reader]
-                    print(data) #Added functionality to print the data
+               with open(user_path, 'r') as file:
+                reader = csv.DictReader(file)
+                for row in reader:
+                    pokemon = {
+                        "name": row["Name"],
+                        "types": [row["Type 1"], row["Type 2"]],
+                        "hp": int(row["HP"]),
+                        "attack": int(row["Attack"]),
+                        "defense": int(row["Defense"]),
+                        "sp_attack": int(row["Sp. Atk"]),
+                        "sp_defense": int(row["Sp. Def"]),
+                        "speed": int(row["Speed"])
+                            
+                    }
+                    data.append(pokemon)
                 return data
             elif user_path.endswith('.json'):
-                with open(user_path, 'r') as file:
-                    data = json.load(file)
-                    print(data) #Added functionality to print the data
+               with open(user_path, 'r') as file:
+                data = json.load(file)
+                print(data) #Added functionality to print the data
                 return data
             else:
                 print("Incorrect file type. File must be a csv or json file.")
         except Exception:
             print(f"Error reading the file: {Exception}. Please try again.")
+      
+
+def individual_entry(data, name):
+    track = 0
+    for item in data:
+        if item["name"].lower() == name.lower():
+            print(f"\nDetails for {item['name']}:")
+            track+=1
+            for key, value in item.items():
+                print(f"{key.capitalize()}: {value}")
+    
+    if track:
+        return
+    else:
+        print(f"No Pokémon found with name '{name}'.")
     
 
 run = True
 
 while(run) : 
+    user_path  ="./all_CSVs/mainpoke/pokemon.csv"
+    data = read_file(user_path)
+    
     print("press h for list of options ")
     selection = input("Enter selection: ")
     if selection == 'h' : 
         print(''' list of options: 
-                1 - option 1 
-                2 - option 2 
+                1 - List all Pokemons
+                2 - List Individual Pokemon
                 3 - option 3 
                 4 - quit ''') # Added the quit option to display.
     elif selection == "1" : 
-        print("opiton 1: Read file ") # Changed option 1 to read file to test reading CSVs
-        read_file()
-    elif selection == "2" : 
-        print("option 2")
+        # list all pokemons
+        if data:
+            print(data) #Added functionality to print the data  
+    elif selection == "2" :
+        if data:
+            name = input("Enter Pokemon's name: ")
+            individual_entry(data, name)
     elif selection == "3" : 
         print(user_path)
-    elif selection == '4' : 
+    elif selection == '4' or selection == 'q' : 
         print("quitting")
         run = False
+    elif selection == "5" : 
+        print("opiton 1: Read file ") # Changed option 1 to read file to test reading CSVs
+       
     else : 
         print("invalid selection")
 
